dbd7b9ccaf4f976f189dcb3566695614598559ca
Mohammed Altujjar <mohammed_.1996@live.com>
2023-03-05 17:58:25 +0400

    JSON

--- src/main/java/com/example/demo/APIController.java
+++ src/main/java/com/example/demo/APIController.java
@@ -5,18 +5,21 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.RequestBody;
-import okhttp3.Response;
+import okhttp3.ResponseBody;
 
 @RestController
 public class APIController {
 	
 	
 	@PostMapping("/api/postInfo")
-	public boolean submitInfo(
+	public Res submitInfo(
 			@RequestParam(name = "email") String email,
 			@RequestParam(name = "firstName") String  firstName,
 			@RequestParam(name = "lastName") String  lastName,
@@ -24,19 +27,26 @@ public class APIController {
 			@RequestParam(name = "phone") int   phone
 			) {
 		try {
-			OkHttpClient client = new OkHttpClient().newBuilder()
-					  .build();
-					MediaType mediaType = MediaType.parse("application/x-www-form-urlencoded");
-					RequestBody body = RequestBody.create(mediaType, "email="+email+"&firstName="+firstName+"&lastName="+lastName+"&password="+password+"&phone="+phone);
-					Request request = new Request.Builder()
-					  .url("https://serviceamedia.com/?Api-Key=DE916C86-566B-8267-AE69-E7019B4E69B9")
-					  .method("POST", body)
-					  .addHeader("Content-Type", "application/x-www-form-urlencoded")
-					  .build();
-					Response response = client.newCall(request).execute();
-					return true ; 
+					OkHttpClient client = new OkHttpClient().newBuilder()
+							  .build();
+							MediaType mediaType = MediaType.parse("application/x-www-form-urlencoded");
+							RequestBody body = RequestBody.create(mediaType, "email="+email+"&firstName="+firstName+"&lastName="+lastName+"&password="+password+"&phone="+phone);
+							Request request = new Request.Builder()
+							  .url("https://serviceamedia.com/api/v2/leads/")
+							  .method("POST", body)
+							  .addHeader("Api-Key", "DE916C86-566B-8267-AE69-E7019B4E69B9")
+							  .addHeader("Content-Type", "application/x-www-form-urlencoded")
+							  .build();
+							ResponseBody  response = client.newCall(request).execute().body();
+
+							String json = response.string() ; 
+							ObjectMapper objectMapper = new ObjectMapper().configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+							Res res = objectMapper.readValue(json, Res.class);
+							
+					return  res ; 
 		}catch(Exception ex ) {
-			return false ; 
+			ex.printStackTrace();
+			return null ; 
 		}
 	}
 
---
+++ src/main/java/com/example/demo/Res.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+
+
+public class Res {
+
+	private Error[] errors ;
+
+	public Error[] getErrors() {
+		return errors;
+	}
+
+	public void setErrors(Error[] errors) {
+		this.errors = errors;
+	}  
+	
+	
+	
+}
